065cf0ff205a55b0a04390d9a2a66c730945d8d9
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2025-07-28 17:57:14 +0300

    add

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -4,6 +4,21 @@ class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
+  Widget dataForCodeOrder(int num , String txt){
+    return Column(
+      children: [
+        Text('$num',style: TextStyle(
+            color: Colors.deepOrange,
+            fontWeight: FontWeight.bold,
+            fontSize: 30
+        ),),
+        Text(txt,style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15
+        ),)
+      ],
+    );
+  }
   Widget build(BuildContext context) {
     return Column(
       children: [
@@ -14,7 +29,15 @@ class ProfileScreen extends StatelessWidget {
             image: DecorationImage(image: AssetImage('assets/images/profile-icon.jpg',
             ),)
           ),
-          )
+          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          dataForCodeOrder(30, 'Orders'),
+          dataForCodeOrder(3, 'Codes'),
+          ],
+        )
+
       ],
     );
   }
